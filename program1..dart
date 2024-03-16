179eb4367466a580317fcc3971eea1c558364f5b
Chebichii-Lab <chebichiiserem@gmail.com>
2024-03-16 18:06:56 +0300

    create a function displaying a one logical sentence

--- program1..dart
+++ program1..dart
@@ -1,2 +1,21 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+// ## Program 1: Display Personal Information
+// Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+class Me {
+  String name;
+  int age;
+  String school;
+  String hobby;
+
+  Me(this.name, this.age, this.school, this.hobby);
+
+  void displayMe() {
+    print('My Name is $name and I am $age years old. The school I attend is $school and one of my favourite hobbies is $hobby.');
+  }
+}
+
+void main() {
+  var me = Me('Natasha Serem', 32, 'PLP Academy', 'Swimming');
+
+  me.displayMe();
+}
